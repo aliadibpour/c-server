fd7a9415276188d7dd512b970baf4885248a021d
aliadibpour <ali.adp0101@gmail.com>
2025-03-10 20:12:40 +0330

    add callback for send matchList on the day for client

--- src/live-match-result/live-match-result.getaway.ts
+++ src/live-match-result/live-match-result.getaway.ts
@@ -3,21 +3,17 @@ import {
   WebSocketGateway, 
   OnGatewayConnection, 
   OnGatewayDisconnect, 
-  MessageBody, 
-  ConnectedSocket 
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { LiveMatchResultService } from './live-match-result.service';
 
 @WebSocketGateway({ cors: true })
 export class LiveMatchGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  private dataArray = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango'];
   private liveData: any; 
 
   constructor(private readonly liveMatchResult: LiveMatchResultService) {}
 
   async onModuleInit() {
-    // This runs **once** when the WebSocket gateway is initialized
     this.liveData = await this.liveMatchResult.liveResult();
     console.log('Live Data Initialized:', this.liveData);
   }
@@ -25,11 +21,14 @@ export class LiveMatchGateway implements OnGatewayConnection, OnGatewayDisconnec
   handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}`);
 
-    // Send the stored liveData to the client
-    client.on('send_index', (data: { index: number }) => {
-      const { index } = data;
-      const response = this.dataArray[index] || 'Invalid index';
-      client.emit('receive_data', { index, value: this.liveData });
+    client.on('live-match', (day, callback) => {
+      console.log(day);
+      try {
+        callback({matchList: this.liveData[+day]})
+      } catch (error) {
+        console.log(error)
+      }
+      //client.emit('live-match-list', { matchList: this.liveData[3] });
     });
   }
 
